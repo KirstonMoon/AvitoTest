8dd26e8a36e40b352212bc5768024603aa8f4d84
KirillMagerya <fresno.super@gmail.com>
2021-01-02 19:49:33 +0700

    создал проект

---
+++ AvitoTest/AvitoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTest
+//
+//  Created by Kirill Magerya on 02.01.2021.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
