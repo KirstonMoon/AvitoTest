a2d0123e2ae26999fd28c6cabd8890b0c2292fdb
KirillMagerya <fresno.super@gmail.com>
2021-01-05 04:41:42 +0700

    перенес view в mainViewController в отдельный класс

--- AvitoTest/AvitoTest/MainModule/CollectionView/MainViewController.swift
+++ AvitoTest/AvitoTest/MainModule/CollectionView/MainViewController.swift
@@ -13,113 +13,94 @@ protocol MainDisplayLogic: class {
 
 final class MainViewController: UIViewController {
     
-    var selectedIndex: Int?
-    var selectedTitleString: String?
+    override func loadView() {
+        view = MainView()
+    }
+    
+    private var mainView: MainView {
+        guard let view = view as? MainView else { fatalError("Could not create a view") }
+        return view
+    }
+    
+    static var selectedIndex: Int?
+    static var selectedTitleString: String?
     
     var interactor: MainInteractorLogic?
     
     private var fetchedDataArray: [List]?
     private var images: [UIImage]?
     
-    var collectionview: UICollectionView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        interactor?.fetchDataFromJson()
-        setupView()
-    }
-    
-    private func setupView() {
-        setupActivityIndicator()
-        setupCloseMarkView()
-        setupTitleLabel()
-        setupChooseButton()
         addTargetToChooseButton()
-        setupCollectionView()
+        setupDelegateAndDataSource()
+        interactor?.fetchDataFromJson()
     }
     
-    private func showLayoutElements() {
-        closeMarkView.isHidden = false
-        titleLabel.isHidden = false
-        chooseButton.isHidden = false
+    private func setupDelegateAndDataSource() {
+        mainView.collectionview.delegate = self
+        mainView.collectionview.dataSource = self
     }
-    
-    //MARK: - UI Elements
-    
-    let activityIndicator: UIActivityIndicatorView = {
-        let activityIndicator = UIActivityIndicatorView(style: .large)
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.color = .black
-        return activityIndicator
-    }()
-    
-    let closeMarkView: UIImageView = {
-        let theImageView = UIImageView()
-        theImageView.image = UIImage(named: "CloseIconTemplate")
-        theImageView.translatesAutoresizingMaskIntoConstraints = false
-        return theImageView
-    }()
-    
-
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        label.font = .boldSystemFont(ofSize: 27)
-        return label
-    }()
-    
-    let chooseButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 8
-        button.backgroundColor = #colorLiteral(red: 0, green: 0.6754948497, blue: 1, alpha: 1)
-        return button
-    }()
-    
 }
 
 extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return fetchedDataArray?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionview.dequeueReusableCell(withReuseIdentifier: MainCollectionViewCell.cellId, for: indexPath) as! MainCollectionViewCell
+        let cell = mainView.collectionview.dequeueReusableCell(withReuseIdentifier: MainCollectionViewCell.cellId, for: indexPath) as! MainCollectionViewCell
         cell.titlelabel.text = fetchedDataArray?[indexPath.row].title
         cell.textLabel.text = fetchedDataArray?[indexPath.row].listDescription
         cell.priceLabel.text = fetchedDataArray?[indexPath.row].price
-        cell.titleImageView.image = self.images?[indexPath.row]
+        cell.titleImageView.image = images?[indexPath.row]
         
-        if indexPath.row == selectedIndex {
+        if indexPath.row == MainViewController.selectedIndex {
             cell.checkMarkButton.isHidden = false
-            self.selectedTitleString = cell.titlelabel.text
+            MainViewController.selectedTitleString = cell.titlelabel.text
         } else {
             cell.checkMarkButton.isHidden = true
         }
-        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        selectedIndex = indexPath.row
-        collectionview.reloadData()
+        MainViewController.selectedIndex = indexPath.row
+        mainView.collectionview.reloadData()
     }
 }
 
 extension MainViewController: MainDisplayLogic {
 
     func displayData(viewModel: DataViewModel) {
-        self.fetchedDataArray = viewModel.dataArray
-        self.images = viewModel.dataImagesArray
-        self.titleLabel.text = viewModel.title
-        self.chooseButton.setTitle(viewModel.buttonTitle, for: .normal)
-        collectionview.reloadData()
-        activityIndicator.stopAnimating()
-        showLayoutElements()
+        fetchedDataArray = viewModel.dataArray
+        images = viewModel.dataImagesArray
+        mainView.titleLabel.text = viewModel.title
+        mainView.chooseButton.setTitle(viewModel.buttonTitle, for: .normal)
+        mainView.collectionview.reloadData()
+        mainView.activityIndicator.stopAnimating()
+        mainView.showUIelements()
     }
 }
 
+private extension MainViewController {
+    @objc func didPressChooseButton() {
+        
+        if MainViewController.selectedTitleString == nil {
+            let alert = UIAlertController(title: "Выберете один из вариантов", message: nil, preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: MainViewController.selectedTitleString, message: nil, preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func addTargetToChooseButton() {
+        mainView.chooseButton.addTarget(self, action: #selector(didPressChooseButton), for: .touchUpInside)
+    }
+}
